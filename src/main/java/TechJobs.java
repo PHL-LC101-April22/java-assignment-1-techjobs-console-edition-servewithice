ca4c09def366b21c81688060ffb19923efe2d050
arielmitchell <ariel.l.makowski@gmail.com>
2022-07-29 17:04:51 -0400

    Test two take two

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -77,7 +77,7 @@ public class TechJobs {
         Boolean validChoice = false;
         String[] choiceKeys = new String[choices.size()];
 
-        // Put the choices in an ordered structure so we can
+        // Put the choices in an ordered structure, so we can
         // associate an integer with each one
         int i = 0;
         for (String choiceKey : choices.keySet()) {
@@ -123,7 +123,7 @@ public class TechJobs {
         String bottomAsterisks = "*****";
 
         if (someJobs.size() == 0) {
-            System.out.println("No Results");
+            System.out.print("No Results");
         } else {
             for (HashMap<String, String> job : someJobs) {
                 ArrayList<String> printJob = new ArrayList<>();
